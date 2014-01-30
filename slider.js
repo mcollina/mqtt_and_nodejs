3a43c98a94a00e1d83c60be82db4d2c049d37bd9
Matteo Collina <matteo.collina@gmail.com>
2014-01-30 17:22:49 +0100

    Added script autorun capabilities.

---
+++ slider.js
@@ -0,0 +1,50 @@
+
+if (!process.env.DEBUG)
+  process.env.DEBUG = 'blue'
+
+var debug = require('debug')('blue')
+  , SensorTag = require('sensortag')
+  , press = require('mac-key-press')
+  , leftKey = 123
+  , rightKey = 124
+  , client = require('mqtt').createClient()
+
+debug('searching for a sensor tag')
+
+SensorTag.discover(function(sensorTag) {
+  debug('discovered', sensorTag.uuid)
+
+  sensorTag.connect(function() {
+    debug('connected')
+    sensorTag.discoverServicesAndCharacteristics(function() {
+      debug('discovered all characteristics')
+      sensorTag.notifySimpleKey(function() {
+        debug('key press correctly setted up')
+      })
+
+      sensorTag.enableHumidity(function() {
+        sensorTag.notifyHumidity(function() {
+          debug('umidity correctly setted up')
+        })
+      })
+    })
+  })
+
+  sensorTag.on('simpleKeyChange', function(left, right) {
+    if (left) {
+      debug('left pressed')
+      //press(leftKey)
+      client.publish('deck/prev');
+    } else if (right) {
+      debug('right pressed')
+      client.publish('deck/next');
+      //press(rightKey)
+      press(36) // return
+    }
+  })
+
+  sensorTag.on('humidityChange', function(temperature, humidity) {
+    client.publish('sensortag/temperature', '' + temperature)
+    client.publish('sensortag/humidity', '' + humidity)
+  })
+})
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,32 @@
+
+var mqtt = require("mows");
+
+mqtt.createClientBase = mqtt.createClient;
+mqtt.createClient = function(opts) {
+  return this.createClientBase("ws://localhost:3000", opts);
+};
+
+(function() {
+});
+
+bespoke.plugins.mqtt = function(deck) {
+  var slider = mqtt.createClient();
+  slider.subscribe('deck/next')
+  slider.subscribe('deck/prev')
+
+  slider.on('message', function(topic) {
+    var command = topic.replace('deck/', '')
+    console.log('received', command);
+    if (deck[command])
+      deck[command]()
+  });
+};
+
 bespoke.from('article', {
+  mqtt: true,
   keys: true,
   touch: true,
+  run: true,
   bullets: 'li, .bullet',
   scale: true,
   hash: true,
