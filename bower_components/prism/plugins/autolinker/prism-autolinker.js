b28a9c0b3f242809ee1f59e3b9ea23bc0ccf8ec4
Matteo Collina <hello@matteocollina.com>
2016-01-22 10:10:41 +0100

    Generated by generator-bespoke

--- bower_components/prism/plugins/autolinker/prism-autolinker.js
+++ bower_components/prism/plugins/autolinker/prism-autolinker.js
@@ -1,41 +1,50 @@
 (function(){
 
-if (!self.Prism) {
+if (
+	typeof self !== 'undefined' && !self.Prism ||
+	typeof global !== 'undefined' && !global.Prism
+) {
 	return;
 }
 
-var url = /\b([a-z]{3,7}:\/\/|tel:)[\w-+%~/.:]+/,
+var url = /\b([a-z]{3,7}:\/\/|tel:)[\w\-+%~/.:#=?&amp;]+/,
     email = /\b\S+@[\w.]+[a-z]{2}/,
     linkMd = /\[([^\]]+)]\(([^)]+)\)/,
     
 	// Tokens that may contain URLs and emails
     candidates = ['comment', 'url', 'attr-value', 'string'];
 
-for (var language in Prism.languages) {
-	var tokens = Prism.languages[language];
-	
-	Prism.languages.DFS(tokens, function (type, def) {
-		if (candidates.indexOf(type) > -1) {
+Prism.hooks.add('before-highlight', function(env) {
+	// Abort if grammar has already been processed
+	if (!env.grammar || env.grammar['url-link']) {
+		return;
+	}
+	Prism.languages.DFS(env.grammar, function (key, def, type) {
+		if (candidates.indexOf(type) > -1 && Prism.util.type(def) !== 'Array') {
 			if (!def.pattern) {
-				def = this[type] = {
+				def = this[key] = {
 					pattern: def
 				};
 			}
-			
+
 			def.inside = def.inside || {};
-			
+
 			if (type == 'comment') {
 				def.inside['md-link'] = linkMd;
 			}
-			
-			def.inside['url-link'] = url;
+			if (type == 'attr-value') {
+				Prism.languages.insertBefore('inside', 'punctuation', { 'url-link': url }, def);
+			}
+			else {
+				def.inside['url-link'] = url;
+			}
+
 			def.inside['email-link'] = email;
 		}
 	});
-	
-	tokens['url-link'] = url;
-	tokens['email-link'] = email;
-}
+	env.grammar['url-link'] = url;
+	env.grammar['email-link'] = email;
+});
 
 Prism.hooks.add('wrap', function(env) {
 	if (/-link$/.test(env.type)) {
@@ -43,7 +52,7 @@ Prism.hooks.add('wrap', function(env) {
 		
 		var href = env.content;
 		
-		if (env.type == 'email-link') {
+		if (env.type == 'email-link' && href.indexOf('mailto:') != 0) {
 			href = 'mailto:' + href;
 		}
 		else if (env.type == 'md-link') {
